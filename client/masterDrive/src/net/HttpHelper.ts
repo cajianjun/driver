f0411cd4376913de1b91baffcc14c71b1337d3e5
caojianjun <1030833228@qq.com>
2017-10-12 07:46:00 +0800

    no message

---
+++ client/masterDrive/src/net/HttpHelper.ts
@@ -0,0 +1,32 @@
+module net{
+    export var http:HttpHelper;
+    class HttpHelper {
+
+        private host:string = "http://localhost:9001/gamecenter";
+
+        public constructor() {
+            
+        }
+
+        public postReq(route:string,data:any,callback:Function){
+            var request = new egret.HttpRequest();
+            request.responseType = egret.HttpResponseType.TEXT;
+            request.open(this.host,egret.HttpMethod.POST);
+            request.setRequestHeader("Content-Type", "application/json");
+            var onSuccess = function (event:egret.Event) {
+                request.removeEventListener(egret.Event.COMPLETE, onSuccess, self);
+                request.removeEventListener(egret.IOErrorEvent.IO_ERROR, onError, self);  
+                var request = <egret.HttpRequest>event.currentTarget;
+                console.log("get data : ",request.response);
+            };
+            var onError = function(event:egret.Event){
+                request.removeEventListener(egret.Event.COMPLETE, onSuccess, self);
+                request.removeEventListener(egret.IOErrorEvent.IO_ERROR, onError, self);
+                console.log(event);
+            }
+            request.send();
+            request.addEventListener(egret.Event.COMPLETE,onSuccess,this);
+            request.addEventListener(egret.IOErrorEvent.IO_ERROR,onError,this);
+        }
+    }
+}
---
+++ client/masterDrive/src/net/Route.ts
@@ -0,0 +1,6 @@
+module net {
+	export class Route {
+		public static R_GET_TOP10:string = "";
+		public static R_POST_SCORE:string = "";
+	}
+}
